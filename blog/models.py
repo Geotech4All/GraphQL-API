17367cb7eba99e4cdac8566d80cefba7186f1f51
Ezekiel Victor <codeepoch@gmail.com>
2022-12-04 02:25:06 +0100

    fixed timedelta issue

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,9 @@ from datetime import timedelta
 from django.contrib.auth import get_user_model #type: ignore
 from django.db import models #type: ignore
 
+
 User = get_user_model()
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -11,18 +13,19 @@ class Post(models.Model):
     body = models.TextField(max_length=700, null=False, blank=False)
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
-    read_length = models.DurationField(null=True, blank=True)
+    read_length = models.FloatField()
     date_added = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.pk} - {self.title}"
 
-    def _set_read_length(self) -> timedelta:
+    def _set_read_length(self) -> float:
         word_count = len(str(self.body).split())
         avg_time_per_word = timedelta(milliseconds=200)
         read_length = avg_time_per_word * word_count
-        return read_length
+        read_length_in_seconds = read_length.total_seconds()
+        return read_length_in_seconds
 
     def save(self, *args, **kwargs) -> None:
         self.read_length = self._set_read_length()
