ae0cc2b9689d1d08893887bd83c2d4f9088d0783
Ezekiel Victor <codeepoch@gmail.com>
2022-12-08 15:20:55 +0100

    preparing for railway

--- podcast/validators.py
+++ podcast/validators.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, Tuple
+from typing import Any
 from django.core.exceptions import ValidationError
 import magic
 from django.utils.deconstruct import deconstructible
