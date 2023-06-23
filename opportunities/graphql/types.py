2adaa87ecb2a3e06473223b26c6025b4e1294904
Ezekiel Victor <codeepoch@gmail.com>
2023-06-23 21:09:33 +0100

    updated opportunity type

--- opportunities/graphql/types.py
+++ opportunities/graphql/types.py
@@ -1,14 +1,17 @@
 import graphene
 from graphene_django import DjangoObjectType
+from assets.graphql.types import TagType
 
 
 from opportunities.models import Opportunity
 
 class OpportunityType(DjangoObjectType):
     opportunity_id = graphene.ID()
+    tags = graphene.List(TagType);
+
     class Meta:
         model = Opportunity
-        fields = ("title", "content", "description", "tags", "date_added", "location", "last_updated")
+        fields = ("title", "content", "description", "date_added", "location", "last_updated")
         filter_fields = {"tags__id": ["in"], "location__id": ["exact"]}
         interfaces = (graphene.relay.Node, )
 
@@ -17,3 +20,8 @@ class OpportunityType(DjangoObjectType):
         if isinstance(self, Opportunity):
             return self.pk
         return None
+
+    def resolve_tags(self, _):
+        if isinstance(self, Opportunity):
+            return self.tags
+        return []
