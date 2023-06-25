5e9843911c92e592e95f6a717bc3c4b65f99d278
Ezekiel Victor <codeepoch@gmail.com>
2023-06-25 19:45:31 +0100

    added title icontains filter to opportunities

--- opportunities/graphql/types.py
+++ opportunities/graphql/types.py
@@ -6,16 +6,28 @@ from assets.models import Tag
 
 from opportunities.models import Opportunity
 
+
 class OpportunityType(DjangoObjectType):
     opportunity_id = graphene.ID()
-    tags = graphene.List(TagType);
+    tags = graphene.List(TagType)
 
     class Meta:
         model = Opportunity
-        fields = ("title", "content", "description", "organization", "date_added", "location", "last_updated")
-        filter_fields = {"tags__id": ["in"], "location__id": ["exact"]}
-        interfaces = (graphene.relay.Node, )
-
+        fields = (
+            "title",
+            "content",
+            "description",
+            "organization",
+            "date_added",
+            "location",
+            "last_updated",
+        )
+        filter_fields = {
+            "tags__id": ["in"],
+            "location__id": ["exact"],
+            "title": ["icontains"],
+        }
+        interfaces = (graphene.relay.Node,)
 
     def resolve_opportunity_id(self, _):
         if isinstance(self, Opportunity):
